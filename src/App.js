ca2da7db753874c0a226a942d415977e53c495a6
vhawk19 <varunhawk19@gmail.com>
2020-02-11 23:03:08 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component}from 'react';
+import Context from './Context';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Context>
+        <div >
+
+          <h1> Tarun poorimon</h1>
+        </div>
+
+      </Context>);
+
 }
 
 export default App;
---
+++ src/Context.js
@@ -0,0 +1,21 @@
+import React,{createContext,Component} from 'react';
+export const Hooks =createContext();
+
+class Context extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            hi:"hello",
+
+        };
+    }
+    render() {
+        return (
+            <Hooks.Provider value={{...this.state}}>
+                {this.props.children}
+            </Hooks.Provider>
+
+        );
+    }
+}
+export default Context;
