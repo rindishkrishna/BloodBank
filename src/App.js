80c80688bf3dab97850aee5658f99ec47b6af4cd
AFNAN NAVAZ <afnan@pop-os.localdomain>
2020-02-12 02:49:16 +0530

    add chart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Context from './context/Context';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Chart from './Components/subcomponent/chart/Chart'
 
 function App() {
   return (
     <Context>
       <Router>
         <Switch>
+          <Route path="/chart">
+            <Chart />
+          </Route>
           <Route path="/">
             <h1>safasf</h1>
           </Route>
